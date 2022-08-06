0d778d797972f7c03957ced8d2a3741ba37caf35
izakdvlpr <izakdvlpr@gmail.com>
2022-08-05 21:22:54 -0300

    add npcs

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import { Direction } from 'grid-engine';
-import type { GridEngine } from 'grid-engine';
+import type { GridEngine, GridEngineConfig } from 'grid-engine';
 
 export class MainScene extends Phaser.Scene {
   gridEngine!: GridEngine;
@@ -22,17 +22,14 @@ export class MainScene extends Phaser.Scene {
     if (!this.gridEngine) return;
 
     const cloudCityTilemap = this.make.tilemap({ key: 'cloud-city-map' });
-
     cloudCityTilemap.addTilesetImage('Cloud City', 'tiles');
 
     for (let i = 0; i < cloudCityTilemap.layers.length; i++) {
       const layer = cloudCityTilemap.createLayer(i, 'Cloud City', 0, 0);
-
       layer.scale = 3;
     }
 
     const playerSprite = this.add.sprite(0, 0, 'player');
-
     playerSprite.scale = 1.5;
 
     this.cameras.main.startFollow(playerSprite, true);
@@ -41,7 +38,16 @@ export class MainScene extends Phaser.Scene {
       -playerSprite.height,
     );
 
-    const gridEngineConfig = {
+    const npcSprite = this.add.sprite(0, 0, 'player');
+    npcSprite.scale = 1.5;
+
+    const npcSprite1 = this.add.sprite(0, 0, 'player');
+    npcSprite1.scale = 1.5;
+
+    const npcSprite2 = this.add.sprite(0, 0, 'player');
+    npcSprite2.scale = 1.5;
+
+    const gridEngineConfig: GridEngineConfig = {
       characters: [
         {
           id: 'player',
@@ -49,10 +55,34 @@ export class MainScene extends Phaser.Scene {
           walkingAnimationMapping: 6,
           startPosition: { x: 8, y: 8 },
         },
+        {
+          id: 'npc0',
+          sprite: npcSprite,
+          walkingAnimationMapping: 0,
+          startPosition: { x: 5, y: 5 },
+          speed: 3,
+        },
+        {
+          id: 'npc1',
+          sprite: npcSprite1,
+          walkingAnimationMapping: 1,
+          startPosition: { x: 10, y: 10 },
+        },
+        {
+          id: 'npc2',
+          sprite: npcSprite2,
+          walkingAnimationMapping: 3,
+          startPosition: { x: 5, y: 10 },
+          speed: 2,
+        },
       ],
     };
 
     this.gridEngine.create(cloudCityTilemap, gridEngineConfig);
+
+    this.gridEngine.moveRandomly('npc0');
+    this.gridEngine.moveRandomly('npc1', 500);
+    this.gridEngine.moveRandomly('npc2', 1500);
   }
 
   update() {
